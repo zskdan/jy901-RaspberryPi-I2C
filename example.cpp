d5c666bfd77a30991d2895473334221c170549fb
Zakaria ElQotbi <zakaria@elqotbi.com>
2023-07-11 01:09:51 +0200

    fixup build and fix style

--- example.cpp
+++ example.cpp
@@ -2,27 +2,32 @@
 #include"JY901.cpp"
 #include<stdio.h>
 #include<wiringPi.h>
+#include <unistd.h>
 
+int main(void)
+{
+    int fd;
 
-int main(void) {
-	jy901_set_addr( 0x50 );
-	printf("\ninit:%#x\n",jy901_init( &fd ));
-	printf("\ncali:%d\n",jy901_cali());
-	if ( INIT_SUCCESS == jy901_init() ) {
-		angle attitude;
-		accel acceleration;
-		gps gpsd;
-		omega w;
-		jy901_cali();
-		for(;;)
-		{
-			jy901_getAngle( &attitude );
-			jy901_getAccel( &acceleration );
-			jy901_getGPS( &gpsd );
-			jy901_getOmega( &w );
-			printf("attitude:%4f,%4f,%4f\nacceleration:%4f,%4f,%4f\ngps:%8f,%8f,%2f,%2f,%d\nomega:%4f,%4f,%4f\n\n",attitude.roll,attitude.pitch,attitude.yaw,acceleration.ax,acceleration.ay,acceleration.az,gpsd.longti,gpsd.lati,gpsd.gpsYaw,gpsd.accur,gpsd.sat_num,w.wx,w.wy,w.wz);
-		}
+    jy901_set_addr( 0x50 );
+    unsigned char jinit = jy901_init(&fd);
+
+    if ( INIT_SUCCESS == jinit ) {
+	angle attitude;
+	accel acceleration;
+	omega w;
+	jy901_cali();
+	for(;;) {
+	    jy901_getAngle( &attitude );
+	    jy901_getAccel( &acceleration );
+	    jy901_getOmega( &w );
+	    printf("attitude:%f,%f,%f\n"
+		"acceleration:%f,%f,%f\n"
+		"omega:%f,%f,%f\n\n",
+		attitude.roll, attitude.pitch, attitude.yaw,
+		acceleration. ax,acceleration.ay, acceleration.az,
+		w.wx, w.wy, w.wz);
 	}
-	else	printf("Initialization failed, err=%#x\n", jy901_init() );
-
+    } else {
+	printf("Initialization failed, err=%#x\n", jinit );
+    }
 }
